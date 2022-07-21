ea9ea759e3c22cb8e19a099657a72a7d6748007a
boluwatifee1 <boluwatifee4@gmail.com>
2022-07-21 04:38:44 +0100

    view nfts integrted

--- src/views/Tabs.tsx
+++ src/views/Tabs.tsx
@@ -3,6 +3,7 @@ import { useDispatch, useSelector } from "react-redux";
 import solLogo from "../assests/images/solLogo.png"
 import { useMoralis } from "react-moralis";
 import UsePagination from "../toolkits/usePagination";
+import { getNftMetadata, fetchNftUri } from "../store/actions/solana.actions";
 
 type Props = {
     color: string;
@@ -10,13 +11,16 @@ type Props = {
 const Tabs: React.FC<Props> = (props) => {
     const { color } = props;
     const [openTab, setOpenTab] = React.useState(1);
+    const [openSoloNft, setOpenSoloNft] = React.useState(false);
     const portfolio = useSelector((state: any) => state.solana.solPortfolio);
+    const nft = useSelector((state: any) => state.solana.nfts);
+    const nfts  = useSelector((state: any) => state.solana.nft);
     const { authenticate, isAuthenticated, user } = useMoralis();
     const nftsLength = portfolio?.data?.nfts?.length
     const tokensLength = portfolio?.data?.tokens?.length
     const [currentPage, setCurrentPage] = React.useState(1);
     const [itemsPerPage, setItemsPerPage] = React.useState(2);
-
+    const dispatch = useDispatch();
     const indexOfLastItem = currentPage * itemsPerPage;
     const indexOfFirstItem = indexOfLastItem - itemsPerPage;
     const currentItems = portfolio?.data?.tokens?.slice(indexOfFirstItem, indexOfLastItem);
@@ -33,10 +37,20 @@ const Tabs: React.FC<Props> = (props) => {
     const goToFirstPage = () => {
         setCurrentPage(1);
     }
+    const getNftMetadatum = (nftId: string) => {
+        dispatch<any>(getNftMetadata("devnet",nftId));
+            setOpenSoloNft(true);
+
+    }
     useEffect(() => {
         console.log("portfolio", portfolio);
+        console.log("nft", nft);
+        console.log("nfts", nfts);
+        if(nft?.status === 200){
+          dispatch<any>(fetchNftUri(nft?.data?.metaplex?.metadataUri));
+        }
         // dispatch<any>(getNftMetadata("mainnet", "2P5msLMi5one6S3qBsUwsDqutQkzuDuG9ush1xLcxYVN"))
-    }, []);
+    }, [nft, ]);
 
     return (
         <>
@@ -161,7 +175,11 @@ const Tabs: React.FC<Props> = (props) => {
                                 <div className={openTab === 3 ? "block" : "hidden"} id="link3">
                                 <div className="mt-10 flex justify-center items-center ">
                                         <div className=" w-4/6 p-6 shadow-xl  bg-gray-400 rounded-xl bg-clip-padding backdrop-filter backdrop-blur-md bg-opacity-30 border border-violet-400">
-                                            <div className="flex flex-wrap justify-around">
+                                          {
+                                            !openSoloNft ?
+                                            (
+                                                <>
+                                                      <div className="flex flex-wrap justify-around">
                                                 {currentNftItems?.map((item: any, index: number) => {
                                                     return (
                                                         <div className="mx-1 shadow-lg text-left my-2 py-1 p-2 w-1/3 bg-gradient-to-b from-gray-900 via-purple-900 to-violet-600 text-white rounded-md">
@@ -173,7 +191,7 @@ const Tabs: React.FC<Props> = (props) => {
                                                                 <h1 className="text-xs my-2">Associated Nft Address:</h1>
                                                                 <h1 className="text-left text-xs">{item?.associatedTokenAddress}</h1>
                                                             </div>
-                                                            <button className="text-sm uppercase bg-gray-400 rounded bg-clip-padding backdrop-filter backdrop-blur-md bg-opacity-30 border border-violet-400 w-full">
+                                                            <button onClick={() => getNftMetadatum(item?.mint)} className="text-sm uppercase bg-gray-400 rounded bg-clip-padding backdrop-filter backdrop-blur-md bg-opacity-30 border border-violet-400 w-full">
                                                                 View Nft
                                                             </button>
                                                         </div>
@@ -189,6 +207,20 @@ const Tabs: React.FC<Props> = (props) => {
                                                     <h1 onClick={goToNftLastPage} className="cursor-pointer"> → </h1>
                                                 </div>
                                             </div>
+                                                </>
+                                            ) :
+                                            (
+                                                <>
+                                                    <h1 className="text-left cursor-pointer" onClick={()=> setOpenSoloNft(false)}>Back</h1>
+                                                    <div className="flex flex-col justify-center">
+                                                    <h1>Name: {nft?.data?.name}</h1>
+                                                    <div className="flex justify-center my-2">
+                                                    <img src={nfts?.data?.image} className=" w-60 h-60" alt="" />
+                                                    </div>
+                                                    </div>
+                                                </>
+                                            )
+                                          }
                                         </div>
                                     </div>
                                 </div>
